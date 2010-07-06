0428381d327ab7d2209469c05d3c4054b85c73d9
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-07-06 16:17:28 -0700

    add in a dummy organization controller to test that we can override it

--- lib/fluxx_crm.rb
+++ lib/fluxx_crm.rb
@@ -1,2 +1,23 @@
-module FluxxCrm
+require "formtastic" 
+require "active_support" 
+require "will_paginate" 
+require "action_controller"
+require "action_view"
+
+# Some classes need to be required before or after; put those in these lists
+CRM_EXTENSION_CLASSES_TO_PRELOAD = []
+CRM_EXTENSION_CLASSES_TO_POSTLOAD = []
+
+CRM_EXTENSION_CLASSES_TO_NOT_AUTOLOAD = CRM_EXTENSION_CLASSES_TO_PRELOAD + CRM_EXTENSION_CLASSES_TO_POSTLOAD
+CRM_EXTENSION_CLASSES_TO_PRELOAD.each do |filename|
+  require filename
+end
+Dir.glob("#{File.dirname(__FILE__).to_s}/extensions/**/*.rb").map{|filename| filename.gsub /\.rb$/, ''}.
+  reject{|filename| CRM_EXTENSION_CLASSES_TO_NOT_AUTOLOAD.include?(filename) }.each {|filename| require filename }
+CRM_EXTENSION_CLASSES_TO_POSTLOAD.each do |filename|
+  require filename
+end
+
+Dir.glob("#{File.dirname(__FILE__).to_s}/fluxx_crm/**/*.rb").each do |fluxx_crm|
+  require fluxx_crm.gsub /\.rb$/, ''
 end
---
+++ lib/fluxx_crm/engine.rb
@@ -0,0 +1,7 @@
+require "rails"
+require "action_controller"
+
+module FluxxCrm
+  class Engine < Rails::Engine
+  end
+end
