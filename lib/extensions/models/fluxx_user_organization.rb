e907de2ad39477c9223547f8fb8654664ae4ef1c
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-07-14 11:57:13 -0700

    adding tests for user organization, make sure that we clear out primary user organization if its user organization is deleted

--- lib/extensions/models/fluxx_user_organization.rb
+++ lib/extensions/models/fluxx_user_organization.rb
@@ -8,10 +8,14 @@ module FLuxxUserOrganization
     base.validates_presence_of :user_id
     base.validates_presence_of :organization_id
     base.validates_uniqueness_of :organization_id, :scope => :user_id
+    base.has_many :primary_user_organizations_users, :class_name => 'User', :foreign_key => :primary_user_organization_id
     
     base.insta_search
+    base.insta_export
+    base.insta_lock
     
     # If the userorganization was connected as a primary organization, nil out the primary_organization of the use
+    base.before_save :clear_out_related_primary_organizations
     
     base.extend(ModelClassMethods)
     base.class_eval do
@@ -23,5 +27,10 @@ module FLuxxUserOrganization
   end
 
   module ModelInstanceMethods
+    def clear_out_related_primary_organizations
+      primary_user_organizations_users.each do |user|
+        user.update_attributes :primary_user_organization => nil
+      end
+    end
   end
 end
--- test/functional/user_organizations_controller_test.rb
+++ test/functional/user_organizations_controller_test.rb
@@ -5,9 +5,84 @@ class UserOrganizationsControllerTest < ActionController::TestCase
   def setup
     @user1 = User.make
     login_as @user1
+    @org1 = Organization.make
+    @org2 = Organization.make
+    @user_org1 = UserOrganization.make :organization => @org2, :user => @user1
   end
   
-  test "assert truth" do
-    assert true
+  test "should get new" do
+    get :new, :user_id => @user1.id
+    assert_response :success
+  end
+  
+  test "should create user organization" do
+    assert_difference('UserOrganization.count') do
+      post :create, :user_organization => {:organization_id => @org1.id, :user_id => @user1}
+    end
+
+    # Figure out how to determine a 201 and the options therein; some HTTP header in the @response object
+    # assert_redirected_to user_organization_path(assigns(:user_organization))
+    
+    assert_equal @org1, assigns(:user_organization).organization
+    assert_equal @user1, assigns(:user_organization).user
+  end
+
+  test "should get edit" do
+    get :edit, :id => @user_org1.id
+    assert_response :success
+  end
+
+  test "should update organization" do
+    @org3 = Organization.make
+
+    put :update, :id => @user_org1.id, :user_organization => {:organization_id => @org3.id}
+    assert_redirected_to user_organization_path(assigns(:user_organization))
+    assert_equal @org3, assigns(:user_organization).organization
+  end
+
+  test "should not be able to add the same organization to the same user more than once" do
+    post :create, :user_organization => {:organization_id => @org2.id, :user_id => @user1.id}
+
+    assert_nil assigns(:user_organization).id
+  end
+
+  test "should not be able to change a user_org to be the same organization/user as an existing one" do
+    @org4 = Organization.make
+    @user_org2 = UserOrganization.make :organization => @org4, :user => @user1
+    put :update, :id => @user_org2.id, :user_organization => {:organization_id => @org2.id}
+
+    assigns(:user_organization).reload # Check to see what the actual current value in the DB is for the user_org's organization_id
+    assert_equal @org2.id, assigns(:user_organization).organization_id
+  end
+
+  test "should destroy user_organization" do
+    @org4 = Organization.make
+    @user_org2 = UserOrganization.make :organization => @org4, :user => @user1
+    delete :destroy, :id => @user_org2.to_param
+    assert_not_nil @user_org2.reload().deleted_at 
+    assert_redirected_to user_organization_url(@user_org2)
+  end
+
+  test "should destroy user_organization and primary user organization" do
+    @org4 = Organization.make
+    @user_org2 = UserOrganization.make :organization => @org4, :user => @user1
+    @user1.update_attribute :primary_user_organization_id, @user_org2.id
+    
+    delete :destroy, :id => @user_org2.to_param
+    assert_not_nil @user_org2.reload().deleted_at 
+    assert_redirected_to user_organization_url(@user_org2)
+    assert_nil @user1.reload.primary_user_organization_id
+  end
+
+  test "should not be allowed to edit if somebody else is editing" do
+    @user_org1.update_attributes :locked_until => (Time.now + 5.minutes), :locked_by_id => User.make.id
+    get :edit, :id => @user_org1.to_param
+    assert assigns(:not_editable)
+  end
+
+  test "should not be allowed to update if somebody else is editing" do
+    @user_org1.update_attributes :locked_until => (Time.now + 5.minutes), :locked_by_id => User.make.id
+    put :update, :id => @user_org1.to_param, :organization => {}
+    assert assigns(:not_editable)
   end
 end
