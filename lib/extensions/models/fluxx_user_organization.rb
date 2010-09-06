935943dada036b8bcc73c09230fcb572b8186025
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-06 11:37:01 -0700

    ichange name to relationship instead of userOrganization.  Fix geo state for primary user org.  Add extra user org fields.  Fix make primary user relationship.

--- lib/extensions/models/fluxx_user_organization.rb
+++ lib/extensions/models/fluxx_user_organization.rb
@@ -4,6 +4,9 @@ module FluxxUserOrganization
     base.belongs_to :organization
     base.belongs_to :locked_by, :class_name => 'User', :foreign_key => 'locked_by_id'
     base.acts_as_audited({:full_model_enabled => true, :except => [:created_by_id, :updated_by_id, :locked_until, :locked_by_id, :delta]})
+    
+    base.send :attr_accessor, :organization_lookup
+    
 
     base.validates_presence_of :user_id
     base.validates_presence_of :organization_id
@@ -24,6 +27,13 @@ module FluxxUserOrganization
   end
 
   module ModelClassMethods
+    # ESH: hack to rename UserOrganization to Relationship
+    def model_name
+      u = ActiveModel::Name.new UserOrganization
+      u.instance_variable_set '@human', 'Relationship'
+      u
+    end
+    
   end
 
   module ModelInstanceMethods
