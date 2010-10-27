92f9b268fd2d0d8d97e221799c847acdc287272f
Mark Johnson <mark@logicspin.com>
2010-10-27 08:37:35 -1000

    added simple API to model_dsl_workflow for adding and clearing events and states

--- lib/extensions/active_record/model_dsl_workflow.rb
+++ lib/extensions/active_record/model_dsl_workflow.rb
@@ -7,7 +7,12 @@ class ActiveRecord::ModelDslWorkflow < ActiveRecord::ModelDsl
   attr_accessor :events_to_english
   # If this is true, the workflow updates will not execute
   attr_accessor :workflow_disabled
-  
+
+  def initialize model_class
+    super model_class
+    self.states_to_english = HashWithIndifferentAccess.new
+    self.events_to_english = HashWithIndifferentAccess.new
+  end
   
   def state_to_english state_name
     if !state_name.blank? && states_to_english && states_to_english.is_a?(Hash)
@@ -20,4 +25,21 @@ class ActiveRecord::ModelDslWorkflow < ActiveRecord::ModelDsl
       events_to_english[event_name.to_sym]
     end || event_name
   end
+  
+  def add_state_to_english new_state, state_name
+    states_to_english[new_state.to_sym] = state_name
+  end
+
+  def add_event_to_english new_event, event_name
+    events_to_english[new_event.to_sym] = event_name
+  end
+
+  def clear_states_to_english
+    states_to_english.clear
+  end
+
+  def clear_events_to_english
+    events_to_english.clear
+  end
+  
 end
---
+++ test/unit/active_record/model_dsl_workflow_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class ModelDslWorkflowTest < ActiveSupport::TestCase
+  def setup
+    @dsl_workflow = ActiveRecord::ModelDslWorkflow.new Race
+  end
+  
+  test "add state via add_state_to_english" do
+    assert_nil @dsl_workflow.states_to_english
+    @dsl_workflow.add_state_to_english :a_new_state, 'A New State'
+    assert_equal @dsl_workflow.states_to_english[:a_new_state], 'A New State'
+  end
+  test "add event via add_event_to_english" do
+    @dsl_workflow.add_event_to_english :a_new_event, 'A New Event'
+    assert_equal @dsl_workflow.events_to_english[:a_new_event], 'A New Event'
+  end
+
+  test 'clearing states' do
+    @dsl_workflow.clear_states_to_english
+    assert_equal @dsl_workflow.states_to_english, {}
+  end
+  
+  test 'clearing events' do
+    @dsl_workflow.clear_events_to_english
+    assert_equal @dsl_workflow.events_to_english, {}
+  end
+  
+  test 'clearing and adding' do
+    race = Race.new
+    workflow = race.workflow_object
+    assert_equal workflow.states_to_english.size, 4
+    workflow.clear_states_to_english
+    assert_equal workflow.states_to_english.size, 0
+    workflow.add_state_to_english :a_new_state, 'A New State'
+    assert_equal workflow.states_to_english.size, 1
+    assert_equal workflow.state_to_english('a_new_state'), 'A New State'
+  end
+
+end
