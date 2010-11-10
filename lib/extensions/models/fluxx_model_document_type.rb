28a9ea8aac64fb652ac2a430f6cec232d30e39f2
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-11-10 14:28:02 -0800

    switch to model_type in URL for model_document_types

--- lib/extensions/models/fluxx_model_document_type.rb
+++ lib/extensions/models/fluxx_model_document_type.rb
@@ -1,5 +1,5 @@
 module FluxxModelDocumentType
-  SEARCH_ATTRIBUTES = []
+  SEARCH_ATTRIBUTES = [:model_type]
 
   def self.included(base)
     base.belongs_to :created_by, :class_name => 'User', :foreign_key => 'created_by_id'
