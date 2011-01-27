d1f159ab9846102862559b33b0038748bce634fe
Mark Johnson <mark@logicspin.com>
2011-01-27 05:37:18 -1000

    added city to available liquid methods

--- lib/extensions/models/fluxx_organization.rb
+++ lib/extensions/models/fluxx_organization.rb
@@ -1,7 +1,7 @@
 module FluxxOrganization
   include ::URLCleaner
   SEARCH_ATTRIBUTES = [:state, :created_at, :updated_at, :name, :id]
-  LIQUID_METHODS = [:name, :street_address, :street_address2, :country_name, :state_name, :postal_code, :url, :acronym]  
+  LIQUID_METHODS = [:name, :street_address, :street_address2, :city, :state_name, :postal_code, :country_name, :url, :acronym]  
   
   def self.included(base)
     base.has_many :user_organizations
