6ccbb2e7f7c83224f9cc0f4f569c9a5323669b47
Eric Hansen <ehansen486@gmail.com>
2011-05-25 15:33:51 -0700

    alerts - set from address

--- lib/extensions/mailers/fluxx_grant_alert_mailer.rb
+++ lib/extensions/mailers/fluxx_grant_alert_mailer.rb
@@ -3,7 +3,7 @@ module FluxxCrmAlertMailer
 
   instance_methods do
     def alert(recipient, alert, locals={})
-      mail(:to => recipient.is_a?(String) ? recipient : recipient.email,
+      mail(:from => (defined?(DEFAULT_EMAIL_SENDER) && DEFAULT_EMAIL_SENDER ? DEFAULT_EMAIL_SENDER : 'do-not-reply@fluxxlabs.com'), :to => recipient.is_a?(String) ? recipient : recipient.email,
            :subject => alert.liquid_subject('recipient' => recipient),
            :body => alert.liquid_body('recipient' => recipient))
     end
