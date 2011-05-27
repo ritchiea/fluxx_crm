a0d62b0f558f9b19e5d298e6b3d2c26f2a731154
Eric Hansen <ehansen486@gmail.com>
2011-05-26 20:59:15 -0700

    add in a way to adhoc-create documents from templates

--- lib/extensions/controllers/fluxx_model_documents_controller.rb
+++ lib/extensions/controllers/fluxx_model_documents_controller.rb
@@ -6,10 +6,21 @@ module FluxxModelDocumentsController
       insta.template = 'model_document_list'
       insta.icon_style = ICON_STYLE
     end
+
+    base.insta_new ModelDocument do |insta|
+      insta.template = 'model_document_from_template_form'
+      insta.icon_style = ICON_STYLE
+    end
+    
     # The view page will want to pass in the documentable ID and Class
     base.insta_post ModelDocument do |insta|
       insta.pre do |conf|
-        if params[:name]
+        if params[:model_document][:model_document_template_id]
+          # Create a model document based on a template
+          template = ModelDocumentTemplate.find params[:model_document][:model_document_template_id] rescue nil
+          self.pre_model = ModelDocument.create params[:model_document].merge(:document_type => :text, :document_text => template.document, :model_document_template_id => template.id, :document_file_name => template.description) if template
+        end
+        if !self.pre_model && params[:name]
           # Need to grab the file and add it to the model document
           self.pre_model = ModelDocument.new params[:model_document]
           pre_model.model_document_actual_filename = params[:name]
