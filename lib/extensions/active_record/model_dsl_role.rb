9be0ad588bf33cd1963e65d3a44d6dfb19d5e7c9
Eric Hansen <ehansen486@gmail.com>
2011-07-01 09:27:00 +0200

    FLX-1182 Finance Admin User needs ability to edit a transaction add the notion of admin edit

--- lib/extensions/active_record/model_dsl_role.rb
+++ lib/extensions/active_record/model_dsl_role.rb
@@ -1,6 +1,7 @@
 class ActiveRecord::ModelDslRole < ActiveRecord::ModelDsl
   # A mapping of events to roles
   attr_accessor :event_role_mappings
+  attr_accessor :admin_edit_roles
   attr_accessor :extract_related_object_proc
   
   def extract_related_object &block
@@ -10,6 +11,25 @@ class ActiveRecord::ModelDslRole < ActiveRecord::ModelDsl
   def initialize model_class
     super model_class
     self.event_role_mappings = HashWithIndifferentAccess.new
+    self.admin_edit_roles = []
+  end
+  
+  def add_admin_edit_for_roles role
+    if role.is_a? Array
+      role.each {|a_role| admin_edit_roles << a_role.to_s}
+    else
+      self.admin_edit_roles << role.to_s
+    end
+  end
+  
+  def admin_edit_allowed_for_user? user, related_object_model
+    retval = if admin_edit_roles
+      !(admin_edit_roles.select do |role_name|
+        user.has_role?(role_name, related_object_model)
+      end).empty?
+    end
+    
+    retval
   end
   
   def add_event_roles event, related_object, roles
