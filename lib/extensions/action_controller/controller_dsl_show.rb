d10a4d20c34b2ceefe773c68a80ce77668c025f9
Eric Hansen <ehansen486@gmail.com>
2011-07-12 06:31:12 +0200

    pull out edit buttons from show buttons make it so that dsl_show doesn't stomp on the footer if specified

--- lib/extensions/action_controller/controller_dsl_show.rb
+++ lib/extensions/action_controller/controller_dsl_show.rb
@@ -1,7 +1,7 @@
 class ActionController::ControllerDslShow < ActionController::ControllerDsl
   def add_workflow
     if model_class && model_class.public_method_defined?(:current_allowed_events)
-      self.footer_template = 'insta/show_buttons'
+      self.footer_template = 'insta/show_buttons' if self.footer_template.blank?
       self.post do |pair|
         controller_dsl, model = pair
         action_buttons = if model
