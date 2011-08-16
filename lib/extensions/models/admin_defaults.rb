a890ad3c0eac1b31b767e2ee886612f90563dff0
Eric Hansen <ehansen486@gmail.com>
2011-08-16 14:31:11 +0200

    move out of fluxx_crm the admindefault settings

--- lib/extensions/models/admin_defaults.rb
+++ lib/extensions/models/admin_defaults.rb
@@ -9,14 +9,6 @@ class AdminDefaults
   attr_accessor :pre_create 
   
   def initialize
-    self.workflows = [['New Request Workflow', Request.name], ['New Report Workflow', RequestReport.name], ['New Transaction Workflow', RequestTransaction.name]],
-    self.alerts = [['New Report Alert', RequestReportsController.name]],
-    self.roles = [['New Program Role', Program.name]],
-    self.states = [['New Request State', Request.name], ['New Report State', RequestReport.name], ['New Transaction State', RequestTransaction.name]],
-    self.attributes = [['New Request Attribute', Request.name], ['New Report Attribute', RequestReport.name], ['New Transaction Attribute', RequestTransaction.name], ['New LOI Attribute', Loi.name]],
-    self.methods = [['New Request Model Method', Request.name], ['New Report Model Method', RequestReport.name], ['New Transaction Model Method', RequestTransaction.name]],
-    self.validations = [['New Request Model Validation', Request.name], ['New Report Model Validation', RequestReport.name], ['New Transaction Model Validation', RequestTransaction.name]],
-    self.pre_create = ['GrantRequest']
   end
   @@single_defaults = nil
   def self.singleton
