e942d794ee7b68001d24dcbfa56ec53527b9107a
Eric Hansen <ehansen486@gmail.com>
2011-08-24 11:02:08 +0200

    kill audits on geo load

--- lib/tasks.rb
+++ lib/tasks.rb
@@ -28,52 +28,56 @@ namespace :fluxx_crm do
     geo_dir = ENV['geo_dir'] || "#{File.dirname(__FILE__).to_s}/../db/geo"
     p "Processing countries"
     # TODO ESH: FIX THIS; is not ruby 1.9 compatible
-    FasterCSV.foreach("#{geo_dir}/countries.csv", :headers => true) do |row|
+    GeoCountry.without_auditing do
+      FasterCSV.foreach("#{geo_dir}/countries.csv", :headers => true) do |row|
       
-      name = row['Country']
-      fips104 = row['FIPS104']
-      iso2 = row['ISO2']
-      iso3 = row['ISO3']
-      ison = row['ISON']
-      internet = row['Internet']
-      capital = row['Capital']
-      map_reference = row['MapReference']
-      nationality_singular = row['NationalitySingular']
-      nationality_plural = row['NationalityPlural']
-      currency = row['Currency']
-      currency_code = row['CurrencyCode']
-      population = row['Population']
-      title = row['Title']
-      comment = row['Comment']
+        name = row['Country']
+        fips104 = row['FIPS104']
+        iso2 = row['ISO2']
+        iso3 = row['ISO3']
+        ison = row['ISON']
+        internet = row['Internet']
+        capital = row['Capital']
+        map_reference = row['MapReference']
+        nationality_singular = row['NationalitySingular']
+        nationality_plural = row['NationalityPlural']
+        currency = row['Currency']
+        currency_code = row['CurrencyCode']
+        population = row['Population']
+        title = row['Title']
+        comment = row['Comment']
 
-      country = GeoCountry.find_by_fips104 fips104
-      if country
-        country.update_attributes :name => name, :fips104 => fips104, :iso2 => iso2, :iso3 => iso3, :ison => ison, :internet => internet, 
-           :capital => capital, :map_reference => map_reference, :nationality_singular => nationality_singular, :nationality_plural => nationality_plural, 
-           :currency => currency, :currency_code => currency_code, :population => population, :title => title, :comment => comment
-      else
-        GeoCountry.create :name => name, :fips104 => fips104, :iso2 => iso2, :iso3 => iso3, :ison => ison, :internet => internet, 
-           :capital => capital, :map_reference => map_reference, :nationality_singular => nationality_singular, :nationality_plural => nationality_plural, 
-           :currency => currency, :currency_code => currency_code, :population => population, :title => title, :comment => comment
+        country = GeoCountry.find_by_fips104 fips104
+        if country
+          country.update_attributes :name => name, :fips104 => fips104, :iso2 => iso2, :iso3 => iso3, :ison => ison, :internet => internet, 
+             :capital => capital, :map_reference => map_reference, :nationality_singular => nationality_singular, :nationality_plural => nationality_plural, 
+             :currency => currency, :currency_code => currency_code, :population => population, :title => title, :comment => comment
+        else
+          GeoCountry.create :name => name, :fips104 => fips104, :iso2 => iso2, :iso3 => iso3, :ison => ison, :internet => internet, 
+             :capital => capital, :map_reference => map_reference, :nationality_singular => nationality_singular, :nationality_plural => nationality_plural, 
+             :currency => currency, :currency_code => currency_code, :population => population, :title => title, :comment => comment
+        end
       end
     end
 
-    p "Processing States"
-    FasterCSV.foreach("#{geo_dir}/states.csv", :headers => true) do |row|
-      country_name = row['country']
-      region_id = row['region_id']
-      region_name = row['region_name']
-      country = GeoCountry.find_by_iso2 country_name
-      country = GeoCountry.find_by_fips104 country_name unless country
-      if country
-        geo_state = GeoState.where(:geo_country_id => country.id, :fips_10_4 => region_id).first
-        if geo_state
-          geo_state.update_attributes :name => region_name
+    GeoState.without_auditing do
+      p "Processing States"
+      FasterCSV.foreach("#{geo_dir}/states.csv", :headers => true) do |row|
+        country_name = row['country']
+        region_id = row['region_id']
+        region_name = row['region_name']
+        country = GeoCountry.find_by_iso2 country_name
+        country = GeoCountry.find_by_fips104 country_name unless country
+        if country
+          geo_state = GeoState.where(:geo_country_id => country.id, :fips_10_4 => region_id).first
+          if geo_state
+            geo_state.update_attributes :name => region_name
+          else
+            GeoState.create :name => region_name, :geo_country_id => country.id, :fips_10_4 => region_id
+          end
         else
-          GeoState.create :name => region_name, :geo_country_id => country.id, :fips_10_4 => region_id
+          raise Exception.new "Could not find a country with an iso2 #{country_name}"
         end
-      else
-        raise Exception.new "Could not find a country with an iso2 #{country_name}"
       end
     end
   end
@@ -85,33 +89,35 @@ namespace :fluxx_crm do
 
     p "Processing cities"
     # TODO ESH: FIX THIS; is not ruby 1.9 compatible
-    FasterCSV.foreach("#{geo_dir}/GeoLiteCity-Location.csv", :headers => true) do |row|
-      loc_id = row['locId']
-      country_name = row['country']
-      region_id = row['region']
-      city_name = row['city']
-      postal_code = row['postalCode']
-      latitude = row ['latitude']
-      longitude = row ['longitude']
-      metro_code = row['metroCode']
-      area_code = row['areaCode']
-      unless city_name.blank? || ['01', 'A1', 'A2'].include?(country_name)
-        country = GeoCountry.find_by_iso2 country_name
-        country = GeoCountry.find_by_fips104 country_name unless country
-        if country
-          geo_state = GeoState.where(:geo_country_id => country.id, :fips_10_4 => region_id).first
+    GeoCity.without_auditing do
+      FasterCSV.foreach("#{geo_dir}/GeoLiteCity-Location.csv", :headers => true) do |row|
+        loc_id = row['locId']
+        country_name = row['country']
+        region_id = row['region']
+        city_name = row['city']
+        postal_code = row['postalCode']
+        latitude = row ['latitude']
+        longitude = row ['longitude']
+        metro_code = row['metroCode']
+        area_code = row['areaCode']
+        unless city_name.blank? || ['01', 'A1', 'A2'].include?(country_name)
+          country = GeoCountry.find_by_iso2 country_name
+          country = GeoCountry.find_by_fips104 country_name unless country
+          if country
+            geo_state = GeoState.where(:geo_country_id => country.id, :fips_10_4 => region_id).first
 
-          geo_state_id = (geo_state ? geo_state.id : nil)
-          city = GeoCity.where(:name => city_name, :geo_state_id => geo_state_id, :geo_country_id => country.id).first
-          if city
-            city.update_attributes :postalCode => postal_code, :latitude => latitude, :longitude => longitude, :metro_code => metro_code, :area_code => area_code,
-              :geo_country_id => country.id, :geo_state_id => geo_state_id, :original_id => loc_id
+            geo_state_id = (geo_state ? geo_state.id : nil)
+            city = GeoCity.where(:name => city_name, :geo_state_id => geo_state_id, :geo_country_id => country.id).first
+            if city
+              city.update_attributes :postalCode => postal_code, :latitude => latitude, :longitude => longitude, :metro_code => metro_code, :area_code => area_code,
+                :geo_country_id => country.id, :geo_state_id => geo_state_id, :original_id => loc_id
+            else
+              GeoCity.create :name => city_name, :postalCode => postal_code, :latitude => latitude, :longitude => longitude, :metro_code => metro_code, :area_code => area_code,
+                :geo_country_id => country.id, :geo_state_id => geo_state_id, :original_id => loc_id
+            end
           else
-            GeoCity.create :name => city_name, :postalCode => postal_code, :latitude => latitude, :longitude => longitude, :metro_code => metro_code, :area_code => area_code,
-              :geo_country_id => country.id, :geo_state_id => geo_state_id, :original_id => loc_id
+            p "Could not find a country with an iso2 #{country_name} for row=#{row.inspect}"
           end
-        else
-          p "Could not find a country with an iso2 #{country_name} for row=#{row.inspect}"
         end
       end
     end
