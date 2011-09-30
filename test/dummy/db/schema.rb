07121f6b19fcb1fae09f304fb0e91f62c5a488dc
Eric Hansen <ehansen486@gmail.com>
2011-09-30 12:45:21 -0700

    FLX-1837 Geo_States in Fluxx Sales has no data in the abbreviation column this is true across the board, this should fix it

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110929175427) do
+ActiveRecord::Schema.define(:version => 20110930194440) do
 
   create_table "alert_emails", :force => true do |t|
     t.string   "mailer_method"
