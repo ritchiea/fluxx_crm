921140ab551e33edaf01c4178ef8d22aac6552e4
Eric Hansen <ehansen486@gmail.com>
2012-01-31 16:26:53 -0800

    FLX-2410 Email Notifications > Change reply-to address from "do-not-reply@fluxxlabs.com" to client-determined email address

--- lib/extensions/mailers/fluxx_grant_alert_mailer.rb
+++ lib/extensions/mailers/fluxx_grant_alert_mailer.rb
@@ -2,8 +2,12 @@ module FluxxCrmAlertMailer
   extend FluxxModuleHelper
 
   instance_methods do
+    def from_email_address
+      Fluxx.config(:from_email_address)
+    end
+    
     def alert(recipient, alert, locals={})
-      mail(:from => (defined?(DEFAULT_EMAIL_SENDER) && DEFAULT_EMAIL_SENDER ? DEFAULT_EMAIL_SENDER : 'do-not-reply@fluxxlabs.com'), :to => recipient.is_a?(User) ? recipient.mailer_email : recipient.to_s, 
+      mail(:from => 'do-not-reply@fluxxlabs.com', :reply_to => from_email_address, :to => recipient.is_a?(User) ? recipient.mailer_email : recipient.to_s, 
            :cc => alert.cc_emails, :bcc => alert.bcc_emails,
            :subject => alert.liquid_subject(locals.merge('recipient' => recipient))) do |format|
              format.html { render :text => alert.liquid_body(locals.merge('recipient' => recipient)) }
@@ -12,7 +16,7 @@ module FluxxCrmAlertMailer
     
     def alert_grouped(recipient, alert, locals={})
       
-      mail(:from => (defined?(DEFAULT_EMAIL_SENDER) && DEFAULT_EMAIL_SENDER ? DEFAULT_EMAIL_SENDER : 'do-not-reply@fluxxlabs.com'), :to => recipient.is_a?(User) ? recipient.mailer_email : recipient.to_s,
+      mail(:from => 'do-not-reply@fluxxlabs.com', :reply_to => from_email_address, :to => recipient.is_a?(User) ? recipient.mailer_email : recipient.to_s,
            :subject => alert.liquid_subject(locals.merge('recipient' => recipient))) do |format|
              format.html { render :text => alert.liquid_body(locals.merge('recipient' => recipient)) }
       end
--- lib/extensions/mailers/fluxx_user_mailer.rb
+++ lib/extensions/mailers/fluxx_user_mailer.rb
@@ -2,14 +2,16 @@ module FluxxUserMailer
   extend FluxxModuleHelper
 
   instance_methods do
+    def from_email_address
+      Fluxx.config(:from_email_address)
+    end
+    
     def forgot_password(user, reset_link)
-      from          (defined?(DEFAULT_EMAIL_SENDER) && DEFAULT_EMAIL_SENDER ? DEFAULT_EMAIL_SENDER : 'do-not-reply@fluxxlabs.com')
-
       @reset_password_link = reset_link
 
       mail(:to => user.mailer_email,
            :subject => "Password Reset",
-           :from => from,
+           :from => 'do-not-reply@fluxxlabs.com', :reply_to => from_email_address,
            :fail_to => from
       ) do |format|
         format.text
@@ -17,14 +19,12 @@ module FluxxUserMailer
     end
     
     def new_user(user)
-      from          (defined?(DEFAULT_EMAIL_SENDER) && DEFAULT_EMAIL_SENDER ? DEFAULT_EMAIL_SENDER : 'do-not-reply@fluxxlabs.com')
-
       @fluxx_link = FluxxManageHost.current_host
       @new_user = user
 
       mail(:to => user.mailer_email,
            :subject => "New User Information",
-           :from => from,
+           :from => 'do-not-reply@fluxxlabs.com', :reply_to => from_email_address,
            :fail_to => from
       ) do |format|
         format.text
