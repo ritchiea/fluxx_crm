24613f06c31c517cbb57e735c11f81b94be6279e
Andrew Lee <drew@intramedia.net>
2012-02-23 14:54:18 -1000

    FLX-2496 Read only UI for profile rules. The system needs to be made client specific before we can provide edit access from the admin interface

--- lib/extensions/controllers/fluxx_user_profiles_controller.rb
+++ lib/extensions/controllers/fluxx_user_profiles_controller.rb
@@ -1,39 +1,39 @@
 module FluxxUserProfilesController
   ICON_STYLE = 'style-user-profiles'
   def self.included(base)
-    base.insta_index Role do |insta|
+    base.insta_index UserProfile do |insta|
       insta.template = 'user_profile_list'
       insta.order_clause = 'name asc, updated_at desc'
       insta.icon_style = ICON_STYLE
       insta.suppress_model_iteration = true
     end
-    base.insta_show Role do |insta|
+    base.insta_show UserProfile do |insta|
       insta.template = 'user_profile_show'
       insta.icon_style = ICON_STYLE
       insta.add_workflow
     end
-    base.insta_new Role do |insta|
+    base.insta_new UserProfile do |insta|
       insta.template = 'user_profile_form'
       insta.icon_style = ICON_STYLE
     end
-    base.insta_edit Role do |insta|
+    base.insta_edit UserProfile do |insta|
       insta.template = 'user_profile_form'
       insta.icon_style = ICON_STYLE
     end
-    base.insta_post Role do |insta|
+    base.insta_post UserProfile do |insta|
       insta.template = 'user_profile_form'
       insta.icon_style = ICON_STYLE
     end
-    base.insta_put Role do |insta|
+    base.insta_put UserProfile do |insta|
       insta.template = 'user_profile_form'
       insta.icon_style = ICON_STYLE
       insta.add_workflow
     end
-    base.insta_delete Role do |insta|
+    base.insta_delete UserProfile do |insta|
       insta.template = 'user_profile_form'
       insta.icon_style = ICON_STYLE
     end
-    base.insta_related Role do |insta|
+    base.insta_related UserProfile do |insta|
       insta.add_related do |related|
       end
     end
